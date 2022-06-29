c9ac2472bc11142b623c488c3cb6efef4ed630fa
haroldtm <harold.torres@lexicondigital.com.au>
2022-06-29 17:56:51 +1000

    code-along complete

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 
 
 fun main() {
-    
+    val direction: Direction = Direction.WEST
+
+    println(direction.name)
+    println(direction.ordinal)
+
+    val parsedDirection = Direction.valueOf("WEST")
+    println(parsedDirection)
+
+    val apiValue = Direction.values()[2]
+    println(apiValue)
+
+    val color = HighlightColor.BLUE
+    println(color.value)
 }
