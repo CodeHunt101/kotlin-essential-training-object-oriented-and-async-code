73bbc5847cd3f5588ff196e89397c75c37416072
Harold Torres Marino <haroldtm55@gmail.com>
2022-07-01 09:12:21 +1000

    code along complete

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 
 
 fun main() {
-    
+    val task1 = Task("a1","Build search feature")
+    val task2 = Task("b2","Add Analytics")
+
+    println(task1.name)
+    println(task2.name)
+
+    val task2Copy = task2.copy(id = "c3")
+
+    val (id, name) = task1
+
+    if (task2Copy == task2) {
+        println("Tasks are the same")
+    } else {
+        println("Tasks are different")
+    }
 }
