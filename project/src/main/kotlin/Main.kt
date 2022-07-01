3f2a2e82befe6418b8144d9726e7bddb81f6b2be
Harold Torres Marino <haroldtm55@gmail.com>
2022-07-01 20:59:24 +1000

    code along complete

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -5,7 +5,18 @@ import kotlin.random.Random
 // calling getString() should never crash
 // errors should be wrapped with StringResult.Failure
 fun main() {
+    when (val result = getString()) {
+        is StringResult.Error -> println("There was an error")
+        is StringResult.Success -> println(result.message)
+    }
+}
 
+fun getString():StringResult {
+    return try {
+        StringResult.Success(getRandomString())
+    } catch (error: IllegalStateException) {
+        StringResult.Error(error)
+    }
 }
 
 fun getRandomString(): String {
